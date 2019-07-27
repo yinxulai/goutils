a162ce68db902a795352d0f818318ec218d86905
yinxulai <yinxulai@hotmail.com>
2019-07-27 19:37:36 +0800

    添加 Restful Package

---
+++ restful/package.go
@@ -0,0 +1,33 @@
+package restful
+
+import (
+	"encoding/json"
+)
+
+// Package Restful 包结构
+type Package struct {
+	Data    interface{} // 数据
+	State   uint16      // 状态码
+	Message string      // 错误信息
+}
+
+// NewString 获取一个 Restful 结构
+func NewString(state uint16, message string, data interface{}) string {
+	return string(NewByte(state, message, data))
+}
+
+// NewByte 获取一个 Restful 结构
+func NewByte(state uint16, message string, data interface{}) []byte {
+	byteData, _ := json.Marshal(New(state, message, data))
+	return byteData
+}
+
+// New 获取一个 Restful 结构
+func New(state uint16, message string, data interface{}) *Package {
+	entity := new(Package)
+	entity.Data = data
+	entity.State = state
+	entity.Message = message
+
+	return entity
+}
