d99daeb371f0518551985c42d0fae19011f288f0
yinxulai <yinxulai@hotmail.com>
2019-08-17 13:49:31 +0800

    优化 config

--- config/default.go
+++ config/default.go
@@ -17,29 +17,14 @@ func Get(key string) (value string, err error) {
 	return defaultService.Get(key)
 }
 
+// AddFile 获取一个配置
+func AddFile(path string) {
+	defaultService.AddFile(path)
+}
+
 // SetStandard 设置定义
 func SetStandard(key string, deft string, required bool, description string) {
 	defaultService.SetStandard(key, deft, required, description)
-}
-
-// AutoLoad 自动加载
-func AutoLoad() {
-	defaultService.AutoLoad()
-}
-
-// LoadFlag 加载启动命令参数
-func LoadFlag() {
-	defaultService.LoadFlag()
-}
-
-// LoadJSONFile 加载文件
-func LoadJSONFile(path string) error {
-	return defaultService.LoadJSONFile(path)
-}
-
-// LoadJSONFiles 加载多个文件
-func LoadJSONFiles(paths ...string) error {
-	return defaultService.LoadJSONFiles(paths...)
 }
 
 // CreateJSONTemplate 写入 json 模版
--- config/service.go
+++ config/service.go
@@ -18,15 +18,17 @@ type standard struct {
 
 func New() *Service {
 	service := new(Service)
+	service.files = []string{}
 	service.data = make(map[string]*string)
 	service.standards = make(map[string]standard)
-	service.SetStandard("config", "./config.json", false, "自定义配置文件")
 	return service
 }
 
 // Service 配置
 type Service struct {
-	checked   bool
+	loaded    bool     // 加载完成
+	checked   bool     // 检查完成
+	files     []string // 待加载的文件
 	data      map[string]*string
 	standards map[string]standard
 	sync.RWMutex
@@ -42,7 +44,13 @@ func (c *Service) Set(key string, value string) {
 // Get 获取一个配置
 func (c *Service) Get(key string) (value string, err error) {
 	if !c.checked {
-		if err = c.Check(); err != nil {
+		if err = c.check(); err != nil {
+			return "", err
+		}
+	}
+
+	if !c.loaded {
+		if err = c.load(); err != nil {
 			return "", err
 		}
 	}
@@ -54,8 +62,25 @@ func (c *Service) Get(key string) (value string, err error) {
 	return *c.data[key], nil
 }
 
-// Check 检查加载到的数据
-func (c *Service) Check() (err error) {
+// check 检查加载到的数据
+func (c *Service) load() (err error) {
+	c.loadFlag() // 先加载命令行参数
+
+	// 读取所有文件
+	for _, filePath := range c.files {
+		// 读取文件
+		err = c.loadFile(filePath)
+		if err != nil {
+			return err
+		}
+	}
+
+	c.loaded = true
+	return nil
+}
+
+// check 检查加载到的数据
+func (c *Service) check() (err error) {
 	for _, standard := range c.standards {
 		if standard.Required && c.data[standard.Key] == nil {
 			panic(fmt.Sprintf("%s is required, %s", standard.Key, standard.Description))
@@ -63,6 +88,58 @@ func (c *Service) Check() (err error) {
 	}
 
 	c.checked = true
+	return nil
+}
+
+// loadFlag 加载启动命令参数
+func (c *Service) loadFlag() {
+	c.RLock()
+	defer c.RUnlock()
+	c.checked = false
+	var configPath string
+	cache := make(map[string]*string)
+
+	flag.StringVar(&configPath, "config", "", "指定配置文件,将覆盖程序内设置")
+
+	for _, standard := range c.standards {
+		var value string
+		cache[standard.Key] = &value
+		flag.StringVar(&value, standard.Key, standard.Default, standard.Description)
+	}
+
+	flag.Parse()
+
+	// 用户指定了,追加进去配置文件
+	if configPath != "" {
+		c.files = append(c.files, configPath)
+	}
+
+	for key, value := range cache {
+		if value != nil && *value != "" {
+			c.data[key] = value
+		}
+	}
+}
+
+// loadFile 加载文件
+func (c *Service) loadFile(path string) error {
+	c.RLock()
+	var err error
+	defer c.RUnlock()
+	c.checked = false
+	var data map[string]*string
+
+	err = file.ReadJSON(path, &data)
+	if err != nil {
+		return err
+	}
+
+	for name, value := range data {
+		if value != nil {
+			c.data[name] = value
+		}
+	}
+
 	return nil
 }
 
@@ -83,84 +160,18 @@ func (c *Service) SetStandard(key string, deft string, required bool, descriptio
 	c.data[stan.Key] = &stan.Default
 }
 
-// AutoLoad TODO: 自动加载
-// 自动查找 .json 文件并尝试解析
-// 全部处理完后检查是否必要参数都已齐全
-func (c *Service) AutoLoad() {
-	c.LoadFlag()
-}
-
-// LoadFlag 加载启动命令参数
-// TODO: 有问题 会覆盖 data 中的数据
-func (c *Service) LoadFlag() {
+// AddFile 加载文件
+func (c *Service) AddFile(path string) {
 	c.RLock()
 	defer c.RUnlock()
-	c.checked = false
-	cache := make(map[string]*string)
-
-	for _, standard := range c.standards {
-		var value string
-		cache[standard.Key] = &value
-		flag.StringVar(&value, standard.Key, standard.Default, standard.Description)
-	}
-
-	flag.Parse()
-
-	for key, value := range cache {
-		if value != nil && *value != "" {
-			c.data[key] = value
-		}
-	}
-
-}
-
-// LoadJSONFile 加载文件
-func (c *Service) LoadJSONFile(path string) error {
-	c.RLock()
-	var err error
-	defer c.RUnlock()
-	c.checked = false
-	var data map[string]*string
-
-	// 如果环境变量指定了文件 则加载指定文件
-	// 程序内的设定则无效 变量等级最高
-	if c.data["config"] != nil {
-		path = *c.data["config"]
-	}
-
-	err = file.ReadJSON(path, &data)
-	if err != nil {
-		return err
-	}
-
-	for name, value := range data {
-		if value != nil {
-			c.data[name] = value
-		}
-	}
-
-	return nil
-}
-
-// LoadJSONFiles 加载多个文件
-func (c *Service) LoadJSONFiles(paths ...string) error {
-	var err error
-	c.checked = false
-	if len(paths) > 0 {
-		for _, path := range paths {
-			if err = c.LoadJSONFile(path); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
+	c.files = append(c.files, path)
 }
 
 // CreateJSONTemplate 写入 json 模版
 func (c *Service) CreateJSONTemplate(path string) error {
 	var err error
 	if !c.checked {
-		if err = c.Check(); err != nil {
+		if err = c.check(); err != nil {
 			return err
 		}
 	}
